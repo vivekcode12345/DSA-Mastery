e8fb80cd833716abdb3e1e9754d244869c38e29c
Vivek Verma <vivek7148verma@gmail.com>
2026-07-09 16:30:34 +0530

    maintained the original stack size

--- 33 Stack /printstack.cpp
+++ 33 Stack /printstack.cpp
@@ -7,10 +7,10 @@ int main(){
     st.push(20);//1
     st.push(30);//2
     st.push(40);//
-    while(st.size()>0){
-        cout<<st.top()<<endl;
-        st.pop();
-    }
+    // while(st.size()>0){
+    //     cout<<st.top()<<endl;
+    //     st.pop();
+    // }
     // now suing extra stack
     stack<int> st1;
     while(st.size()>0){
@@ -18,8 +18,17 @@ int main(){
         st.pop();
         st1.push(x);
     }
+    // pushing again in the original stack.
+    while(st1.size()>0){
+        int x=st1.top();
+        st1.pop();
+        st.push(x); // size 3
+    }
+    // Printing the extra stack
     while(st1.size()>0){
         cout<<st1.top()<<endl;
         st1.pop();
     }
+    cout<<st.size()<<endl;
+    cout<<st1.size()<<endl;
 }
