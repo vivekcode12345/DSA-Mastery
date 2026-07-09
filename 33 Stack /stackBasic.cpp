7ee527ef85e98a8503b6c99d0c0b96214b950691
Vivek Verma <vivek7148verma@gmail.com>
2026-07-09 10:36:05 +0530

    Pop Push STL Added

--- 33 Stack /stackBasic.cpp
+++ 33 Stack /stackBasic.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main(){
-     stack<int> st;
-     cout<<"Size of stack: "<<st.size()<<endl;
-     st.push(10);
-    st.push(20);
-    st.push(30);
+    stack<int> st;
+    cout<<"Size of stack: "<<st.size()<<endl;
+    st.push(10);//0
+    st.push(20);//1
+    st.push(30);//2
+    st.pop();
     cout<<"Size of stack: "<<st.size()<<endl;
 
 }
