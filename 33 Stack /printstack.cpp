7e52dbbd7e10692f693293e96991c54d27717841
Vivek Verma <vivek7148verma@gmail.com>
2026-07-09 16:26:13 +0530

    Extra stack method added

--- 33 Stack /printstack.cpp
+++ 33 Stack /printstack.cpp
@@ -7,10 +7,19 @@ int main(){
     st.push(20);//1
     st.push(30);//2
     st.push(40);//
-     
-    // printing in the reverse order 
     while(st.size()>0){
         cout<<st.top()<<endl;
         st.pop();
+    }
+    // now suing extra stack
+    stack<int> st1;
+    while(st.size()>0){
+        int x=st.top();
+        st.pop();
+        st1.push(x);
+    }
+    while(st1.size()>0){
+        cout<<st1.top()<<endl;
+        st1.pop();
     }
 }
