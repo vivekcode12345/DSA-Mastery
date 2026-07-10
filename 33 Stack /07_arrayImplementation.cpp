e2157008f40570b56ed2012884283bb1034593fc
Vivek Verma <vivek7148verma@gmail.com>
2026-07-10 13:21:47 +0530

    Array Implementation with  stl

--- 33 Stack /07_arrayImplementation.cpp
+++ 33 Stack /07_arrayImplementation.cpp
@@ -1,26 +1,48 @@
 #include<iostream>
 using namespace std;
 class Stack {
-    int arr[x];
-    int top;
+    int arr[5];
+    int idx;
     public:
     Stack() {
-        top = -1;
+        idx = -1;
     }
 
     void push(int x){
-        if(top==x-1){
-            cout<<"Stack Overflow"
+        if(idx==4){
+            cout<<"Stack Overflow"<<endl;
+            return;
         }
-        top++;
-        arr[top]=x;
+        idx++;
+        arr[idx]=x;
     }
 
     void pop(){
-        if(top==-1){
-            cout<<"stack underflow";
+        if(idx==-1){
+            cout<<"Stack Underflow"<<endl;
+            return;
         }
-        top--;
+        idx--;
+    }
+    int top(){
+        if(idx==-1){
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        return arr[idx];
     }
 
-}    
+    int size(){
+        return idx+1;
+    } 
+};
+
+int main(){
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout<<st.top()<<endl;
+    cout<<st.size()<<endl;
+
+}
