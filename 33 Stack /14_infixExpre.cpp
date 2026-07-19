be79a3c736c012605a9c76ed85164484d0c9985c
Vivek Verma <vivek7148verma@gmail.com>
2026-07-19 09:25:36 +0530

    Infix evaluation with expression added

---
+++ 33 Stack /14_infixExpre.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int prio(char ch) {
+    if (ch == '+' || ch == '-')
+        return 1;
+    else
+        return 2;
+}
+
+int solve(int val1, int val2, char ch) {
+    if (ch == '+')
+        return val1 + val2;
+    else if (ch == '-')
+        return val1 - val2;
+    else if (ch == '*')
+        return val1 * val2;
+    else
+        return val1 / val2;
+}
+
+int main() {
+
+    string s = "2+6*4/8-3";
+
+    stack<int> val;
+    stack<char> op;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            val.push(s[i] - '0');
+        }
+        else {
+            if (op.empty() || prio(s[i]) > prio(op.top())) {
+                op.push(s[i]);
+            }
+            else {
+                while (!op.empty() && prio(s[i]) <= prio(op.top())) {
+
+                    char oper = op.top();
+                    op.pop();
+
+                    int v2 = val.top();
+                    val.pop();
+
+                    int v1 = val.top();
+                    val.pop();
+
+                    val.push(solve(v1, v2, oper));
+                }
+                op.push(s[i]);
+            }
+        }
+    }
+
+    while (!op.empty()) {
+        char oper = op.top();
+        op.pop();
+
+        int v2 = val.top();
+        val.pop();
+
+        int v1 = val.top();
+        val.pop();
+
+        val.push(solve(v1, v2, oper));
+    }
+    cout << val.top();
+    return 0;
+}
