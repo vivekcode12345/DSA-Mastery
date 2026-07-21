6fd7dcbc39e4fc6dcfb2151036965bf837a857df
Vivek Verma <vivek7148verma@gmail.com>
2026-07-21 11:33:44 +0530

    postfiToinfix added

---
+++ 33 Stack /18_postfixEvaluation.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int solve(int val1, int val2, char oper) {
+    if (oper == '+') {
+        return val1 + val2;
+    }
+    else if (oper == '-') {
+        return val1 - val2;
+    }
+    else if (oper == '*') {
+        return val1 * val2;
+    }
+    else if (oper == '/') {
+        return val1 / val2;
+    }
+    return 0;
+}
+int main() {
+
+    string s = "79+4*8/3-";
+
+    stack<int> val;
+    for (int i = 0; i < s.length(); i++) {
+
+        if (s[i] >= 48 && s[i] <= 57) {
+            val.push((s[i] - 48));
+        }
+
+        else {
+            int val2=val.top();
+            val.pop();
+            int val1=val.top();
+            val.pop();
+            int ans=solve(val1, val2, s[i]);
+            val.push(ans);
+        }
+    }
+    cout << val.top();
+    return 0;
+}
