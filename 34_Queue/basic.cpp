bdbba1502ec3734dcf53f5a0117e7b31364a0d64
Vivek Verma <vivek7148verma@gmail.com>
2026-07-22 10:50:50 +0530

    Basic Queue Operation added

---
+++ 34_Queue/basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<int>q;
+    // PUSh POP  front=top  
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    cout<<q.front()<<endl;
+    
+
+}
